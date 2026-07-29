31fb733f39c7faa1bf71b5f27c7daee89f607a1d
Javier <javier@nadine.luxor.ly>
2026-07-29 20:52:35 +0200

    chore: remove unused middleware.ts

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(req: Request) {
-  const url = new URL(req.url);
-  if (url.pathname.startsWith("/admin") && !url.pathname.startsWith("/admin-decoy")) {
-    return new NextResponse("Not Found", { status: 404 });
-  }
-  return NextResponse.next();
-}
