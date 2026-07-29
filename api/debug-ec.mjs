ec11292220f1b00ad7e6573ec07537a24f50594c
USMLE-ly <libya.usmle@gmail.com>
2026-07-29 13:38:19 +0200

    debug: check Edge Config contents

---
+++ api/debug-ec.mjs
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const EC_URL = process.env.EDGE_CONFIG;
+  if (!EC_URL) {
+    return res.status(200).json({ error: "No EC_URL" });
+  }
+  try {
+    const resp = await fetch(EC_URL);
+    const data = await resp.json();
+    const keys = Object.keys(data);
+    return res.status(200).json({ keys, sample: data[keys[0]] || null, keyCount: keys.length });
+  } catch (err) {
+    return res.status(200).json({ error: err.message });
+  }
+}
